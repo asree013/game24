14a36bf5d38bb61750254876f78fbb507be97a8c
asree <asree.h@rmutsvmail.com>
2024-05-16 10:47:43 +0700

    update docker-compose

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,9 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+// เพิ่มการรับสัญญาณ SIGTERM
+process.on('SIGTERM', () => {
+  // ทำลายการทำงานที่เหลือ เช่น ปิดการเชื่อมต่อฐานข้อมูลหรือทำการบันทึกข้อมูลก่อนปิดแอปพลิเคชัน
+  process.exit(0); // สิ้นสุด process โดยกำหนด exit code เป็น 0
+});
